deb3f56ef0ef52a8afacd49f40d355d085bbda79
Pedro Collin <67438145+PedroCollin@users.noreply.github.com>
2021-08-25 11:18:40 -0300

    att

--- usuario/views.py
+++ usuario/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
-from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
 from django.core.validators import validate_email
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 def login(request):
-    return render(request,'usuarios/login.html')
+
+    if request.method != 'POST':
+        return render(request, 'usuarios/login.html')
+
+    usuario = request.POST.get('usuario')
+    senha1 = request.POST.get('senha1')
+
+    user = auth.authenticate(request,username=usuario,password=senha1)
+
+    if not user:
+        messages.add_message(messages.ERROR,'Usuário ou senha inválidos')
+        return render(request, 'usuarios/login.html')
+    else:
+        auth.login(request,user)
+        return redirect('dashboard')
 
 def logout(request):
-    return render(request, 'usuarios/logout.html')
+    auth.logout(request)
+    return redirect('home')
 
 def cadastrar(request):
     if request.method != 'POST':
@@ -33,11 +50,30 @@ def cadastrar(request):
     if len(senha1)<6:
         messages.add_message(request, messages.WARNING, 'Senha deve ter no minimo 6 caracter')
         return render(request, 'usuarios/cadastrar.html')
-    
+
     if senha1!=senha2:
         messages.add_message(request, messages.WARNING, 'Senhas diferentes')
         return render(request, 'usuarios/cadastrar.html')
 
+    if User.objects.filter(username=usuario).exists():
+        messages.add_message(request, messages.WARNING, 'Usuario já existe')
+        return render(request, 'usuarios/cadastrar.html')
+
+    if User.objects.filter(email=email).exists():
+        messages.add_message(request, messages.WARNING, 'E-mail já existe')
+        return render(request, 'usuarios/cadastrar.html')
+
+    user = User.objects.create_user(
+        username=usuario,
+        email=email,
+        first_name=nome,
+        last_name=sobrenome,
+        password=senha1
+    )
+    messages.add_message(request,messages.SUCCESS, 'Cadastrado com sucesso!')
+    User.save(request,user)
+    return redirect('login')
+@login_required(redirect_field_name='login')
 def dashboard(request):
     return render(request,'usuarios/dashboard.html')
 
